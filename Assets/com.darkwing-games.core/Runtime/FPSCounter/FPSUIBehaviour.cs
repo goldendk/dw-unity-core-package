8926256aad2c52d2213da94456be9fcc6431ce13
goldendk <goldendk@hotmail.com>
2022-12-10 15:22:00 +0100

    adding position config to dps counter UI

--- Assets/com.darkwing-games.core/Runtime/FPSCounter/FPSUIBehaviour.cs
+++ Assets/com.darkwing-games.core/Runtime/FPSCounter/FPSUIBehaviour.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using DWGames;
+using DWGames.com.darkwing_games.core.Runtime.Util;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -14,14 +16,97 @@ public class FPSUIBehaviour : MonoBehaviour
     private bool lastVisibleSetting = true;
     private UIDocument uiDocument;
 
+
+    [SerializeField] private ScreenLocation screenLocation = ScreenLocation.BOTTOM_LEFT;
+    private ScreenLocation lastScreenLocation = ScreenLocation.BOTTOM_LEFT;
+    private VisualElement rootPanel;
+
     void Start()
     {
-        this.uiDocument = gameObject.GetComponent<UIDocument>();
+        uiDocument = gameObject.GetComponent<UIDocument>();
+        rootPanel = uiDocument.rootVisualElement.Query<VisualElement>("rootPanel");
         label2 = uiDocument.rootVisualElement.Q<Label>("label2");
         DWFPSCounter.FPSUpdateEvent += (fps) => label2.text = fps.ToString();
     }
 
     private void Update()
+    {
+        HandleVisibleFlag();
+        HandleLocationSetting();
+    }
+
+    private void HandleLocationSetting()
+    {
+        if (lastScreenLocation != screenLocation)
+        {
+            lastScreenLocation = screenLocation;
+            switch (screenLocation)
+            {
+                case ScreenLocation.BOTTOM_LEFT:
+                    setDistances(-1, -1, 0, 0);
+                    break;
+                case ScreenLocation.BOTTOM_RIGHT:
+                    setDistances(-1, 0, -0, -1);
+                    break;
+                // case ScreenLocation.MIDDLE_LEFT:
+                //     setDistances(Screen.height / 2, -1, Screen.height / 2, 0);
+                //
+                //     break;
+                // case ScreenLocation.MIDDLE_RIGHT:
+                //     setDistances(Screen.height / 2, 0, Screen.height / 2, -1);
+
+                    break;
+                case ScreenLocation.TOP_LEFT:
+                    setDistances(0, -1, -1, 0);
+                    break;
+                case ScreenLocation.TOP_RIGHT:
+                    setDistances(0, 0, -1, -1);
+                    break;
+            }
+        }
+    }
+
+    private void setDistances(int top, int right, int bottom, int left)
+    {
+        if (top == -1)
+        {
+            rootPanel.style.top = new StyleLength(StyleKeyword.Auto);
+        }
+        else
+        {
+            rootPanel.style.top = new Length(top, LengthUnit.Pixel);
+        }
+
+        if (right == -1)
+        {
+            rootPanel.style.right = new StyleLength(StyleKeyword.Auto);
+        }
+        else
+        {
+            rootPanel.style.right = new Length(top, LengthUnit.Pixel);
+        }
+
+        if (bottom == -1)
+        {
+            rootPanel.style.bottom = new StyleLength(StyleKeyword.Auto);
+        }
+        else
+        {
+            rootPanel.style.bottom = new Length(top, LengthUnit.Pixel);
+        }
+
+        if (left == -1)
+        {
+            rootPanel.style.left = new StyleLength(StyleKeyword.Auto);
+        }
+        else
+        {
+            rootPanel.style.left = new Length(top, LengthUnit.Pixel);
+        }
+    }
+
+
+    private void HandleVisibleFlag()
     {
         if (isVisible != lastVisibleSetting)
         {
---
+++ Assets/com.darkwing-games.core/Runtime/Util/ScreenLocation.cs
@@ -0,0 +1,12 @@
+﻿namespace DWGames.com.darkwing_games.core.Runtime.Util
+{
+    public enum ScreenLocation
+    {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT,
+        // MIDDLE_LEFT,
+        // MIDDLE_RIGHT
+    }
+}
